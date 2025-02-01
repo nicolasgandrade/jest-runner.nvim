9b413a908450993dfdabad3d4f8d75ca89fd6397
Nicolas Guerrero <nicolasguerrerodev@gmail.com>
2025-02-01 20:25:35 -0300

    feat: create the entry point function for the plugin in init.lua

---
+++ lua/jest-runner/init.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+M.run = function(arg)
+  require("jest-runner.commands").run_jest(arg)
+end
+
+vim.api.nvim_create_user_command("Jest", function(opts)
+  M.run(opts.args)
+end, { nargs = 1 })
